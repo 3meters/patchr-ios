b5d7e7c32f6dc367e1ed5132906c76edb6e772f3
Rob MacEachern <rob@robmaceachern.com>
2015-02-23 11:02:19 -0800

    Create/Edit message unwind segue

---
+++ Patchr/PatchDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PatchDetailViewController.swift
+//  Patchr
+//
+//  Created by Rob MacEachern on 2015-02-23.
+//  Copyright (c) 2015 3meters. All rights reserved.
+//
+
+import UIKit
+
+class PatchDetailViewController: UIViewController {
+
+    @IBAction func unwindFromCreateMessage(segue: UIStoryboardSegue) {}
+    
+}
