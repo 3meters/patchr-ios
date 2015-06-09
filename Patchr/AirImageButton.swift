3ed9ce458349d07c4b38fa0ae75412db36616291
Jay Massena <jay@3meters.com>
2015-06-08 21:44:58 -0700

    Add widgets to support new features.

---
+++ Patchr/AirImageButton.swift
@@ -0,0 +1,48 @@
+//
+//  AirImageView.swift
+//  Patchr
+//
+//  Created by Jay Massena on 5/22/15.
+//  Copyright (c) 2015 3meters. All rights reserved.
+//
+
+import UIKit
+
+class AirImageButton: UIButton {
+
+    var activity: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+    var photo: Photo?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    func initialize(){
+        
+        activity.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.addSubview(activity)
+        
+        let xCenterConstraint = NSLayoutConstraint(item: activity, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0)
+        let yCenterConstraint = NSLayoutConstraint(item: activity, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: activity, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 12)
+        let heightConstraint = NSLayoutConstraint(item: activity, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 12)
+        
+        self.addConstraints([xCenterConstraint, yCenterConstraint, widthConstraint, heightConstraint])
+        
+        activity.hidesWhenStopped = true
+    }
+    
+    func startActivity(){
+        activity.startAnimating()
+    }
+    
+    func stopActivity(){
+        activity.stopAnimating()
+    }
+}
---
+++ Patchr/AirTableView.h
@@ -0,0 +1,13 @@
+//
+//  AirTableView.h
+//  Patchr
+//
+//  Created by Jay Massena on 5/8/15.
+//  Copyright (c) 2015 3meters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirTableView : UITableView
+
+@end
