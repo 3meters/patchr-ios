ef895ae6269d012aa7e73208de7a8a19f4389372
Jay Massena <jay@3meters.com>
2015-07-01 18:11:46 -0700

    Ensure correct filter setting

--- Patchr/NearbyTableViewController.swift
+++ Patchr/NearbyTableViewController.swift
@@ -21,6 +21,7 @@ class NearbyTableViewController: PatchTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.activityDate = DataController.instance.activityDate
+        self.filter = .Nearby
     }
     
     override func viewWillAppear(animated: Bool) {
